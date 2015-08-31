42d122518e4d584e465911307772141e51f15b51
TGriffey <tgriffey9389@gmail.com>
2015-08-31 13:25:01 -0500

    Added PinCtrl funcitonality to view all of a user's pins.

--- app/controllers/PinsCtrl.js
+++ app/controllers/PinsCtrl.js
@@ -4,14 +4,32 @@ app.controller("PinsCtrl",
 
   	var ref = new Firebase("https://fake-terest.firebaseio.com/pins");
 
-  	$scope.pins = $firebaseArray(ref);
+  	$scope.allPins = $firebaseArray(ref);
+    console.log($scope.allPins);
 
-			console.log("$scope.pins", $scope.pins);
+	$scope.userId = storage.getVariable("userId");
+    console.log($scope.userId);   
 
-			console.log("$scope.pins[0].title", $scope.pins[0].title); 
+      $scope.allPins.$loaded()
+        .then(function (data) {
 
-		$scope.userId = storage.getVariable("userId");
-      console.log($scope.userId);   
+          console.log(data);
+          console.log($scope.userId);
+          $scope.userPins = [];
+          for (var key in data) {
+            // console.log(data[key].user_id);
+
+            if (data[key].user_id === $scope.userId) { 
+              // console.log(data[key]);
+              $scope.userPins.push(data[key]);
+            }
+          }
+          console.log($scope.userPins);
+        })
+        .catch(function(error) {
+          console.error("Error:", error);
+        });
 
   }
+  
 ]);
