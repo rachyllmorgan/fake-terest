ea7dbdd822fc8bf11cfed2ce7cc72b3398eaffde
TGriffey <tgriffey9389@gmail.com>
2015-09-01 13:06:21 -0500

    Added styling to nav bar, login and views.

--- app/controllers/AuthCtrl.js
+++ app/controllers/AuthCtrl.js
@@ -2,22 +2,52 @@ app.controller("AuthCtrl", ["$scope", "$firebaseAuth", "$q", "storage", "$locati
   function($scope, $firebaseAuth, $q, storage, $location) {
 
     var ref = new Firebase("https://fake-terest.firebaseio.com");
+    console.log(ref.getAuth());
 
-    auth = $firebaseAuth(ref);
+    // auth = $firebaseAuth(ref);
+    var authData = ref.getAuth();
 
-    $scope.login = function(loginType) {
-      
-      $scope.error = null;
-
-        auth.$authWithOAuthPopup(loginType).then(function(authData) {
-          
-            console.log("Logged in as:", authData.uid);
-            storage.addVariable("userId", authData.uid);
-            $location.url('/users/' + authData.uid);
-        }).catch(function(error) {
+    $scope.login = function (loginType) {
+      if (authData === null) {
+      ref.$authWithOAuthPopup(loginType)
+        .then(function (authData) {
+          storage.addVariable("userId", authData.uid);
+          $location.url('/users/' + authData.uid);
+        })
+        .catch(function(error) {
           console.log("Authentication failed:", error);
         });
-      };
+      } else {
+        storage.addVariable("userId", authData.uid);
+        // user_authenticated = true;
+        $location.url('/users/' + authData.uid);
+      }
+    };
+
+    
+    
+
+
+    // $scope.login = function(loginType) {
+      
+    //   $scope.error = null;
+
+    //   // Only show popup if authData is null
+    //   if (userIdObject === null) {
+    //       auth.$authWithOAuthPopup(loginType).then(function(authData) {
+            
+    //           console.log("Logged in as:", authData.uid);
+    //           storage.addVariable("userId", authData.uid);
+    //           $location.url('/users/' + authData.uid);
+    //       }).catch(function(error) {
+    //         console.log("Authentication failed:", error);
+    //       });
+    //     } else {
+    //       storage.addVariable("userId", userIdObject.uid);
+    //     }
+
+    //   // Else store authData.uid in factory
+    // };
 
 }]);
 
